e9718374fdcba5dd237ae29ba153f7f723741905
Clint Losee <clint.losee@rcwilley.com>
2016-11-29 11:51:32 -0700

    Rework for stupid issues causing server to break. Adding POST route.

--- restaurant-api/src/middleware/index.js
+++ restaurant-api/src/middleware/index.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
 export default({ config, db }) => {
-    let api = Router;
+    let api = Router();
 
-    // add middleware
+    // add middleware here
 
     return api;
 }
--- restaurant-api/src/routes/index.js
+++ restaurant-api/src/routes/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import config from '../config';
-import middleware from '../middlware';
-import initializeDB from '../db';
+import initializeDb from '../db';
+import middleware from '../middleware';
+import restaurant from '../controller/restaurant';
 
 let router = express();
 
-//connect to db
-initializeDB(db => {
+// connect to db
+initializeDb(db => {
 
     // internal middleware
     router.use(middleware({ config, db }));
 
     // api routes v1 (/v1)
-
+    router.use('/restaurant', restaurant({ config, db }));
 });
 
 export default router;
