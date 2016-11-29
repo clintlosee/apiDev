8e195a3354f5a3b4b86c872505b905640cf51333
Clint Losee <clint.losee@rcwilley.com>
2016-11-29 14:40:31 -0700

    Added GET, PUT and DELETE routes.

--- restaurant-api/src/controller/restaurant.js
+++ restaurant-api/src/controller/restaurant.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 export default({ config, db }) => {
     let api = Router();
 
-  // '/v1/restaurant/add'
+    // POST - /v1/restaurant/add
     api.post('/add', (req, res) => {
         let newRest = new Restaurant();
         newRest.name = req.body.name;
@@ -18,5 +18,52 @@ export default({ config, db }) => {
             res.json({ restaurant: newRest, message: 'Restaurant saved successfully' });
         });
     });
+
+    // GET all - /v1/restaurant
+    api.get('/', (req, res) => {
+        Restaurant.find({}, (err, restaurants) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurants);
+        });
+    });
+
+    // GET one - /v1/restaurant/:id
+    api.get('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurant);
+        });
+    });
+
+    // PUT - /v1/restaurant/:id
+    api.put('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            restaurant.name = req.body.name;
+            restaurant.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: 'Restaurant successfully updated'});
+            });
+        });
+    });
+
+    // DELETE = /v1/restaurant/:id
+    api.delete('/:id', (req, res) => {
+        Restaurant.remove({ _id: req.params.id}, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'Restaurant successfully deleted'});
+        });
+    });
+
     return api;
 }
